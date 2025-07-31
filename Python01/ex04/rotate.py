696603b5cc6a92491b10d6b496e4c58a75f56bdf
lleciak <leciak.louise@gmail.com>
2025-07-31 14:47:20 +0200

    INIT: EX04 ROTATE

---
+++ Python01/ex04/rotate.py
@@ -0,0 +1,34 @@
+import sys
+from load_image import ft_load
+import matplotlib.pyplot as plt
+import numpy as np
+
+def ft_transpose(img):
+    """
+    Transpose an image without using np.transpose
+    """
+
+def main():
+    try:
+        img = ft_load("animal.jpeg")
+    except AssertionError as e:
+        print("AssertionError:", e, file=sys.stderr)
+        return
+    if img is None:
+        print("image can't be None", file=sys.stderr)
+        return
+    img_zoom = img[100:500, 200:600]
+
+    if img_zoom.shape[-1] == 1:
+        print(f"New shape after slicing: {img_zoom.shape} or {np.squeeze(img_zoom).shape}")
+    else:
+        print(f"New shape after slicing: {img_zoom.shape}")
+
+    print(img_zoom)
+    #img_zoom = np.squeeze(img_zoom)
+    plt.imshow(img_zoom)
+    plt.savefig("zoomed_image.png")
+
+
+if __name__ == "__main__":
+    main()
