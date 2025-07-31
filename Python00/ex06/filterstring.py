c394adc5d723b89346e377815d8592940845eb78
lleciak <leciak.louise@gmail.com>
2025-07-31 14:48:01 +0200

    CHANGE ORGANISATION

---
+++ Python00/ex06/filterstring.py
@@ -0,0 +1,28 @@
+import sys
+from ft_filter import ft_filter
+
+
+def main():
+    """
+    Main block to print the informations on the inpt
+    """
+    try:
+        if (len(sys.argv) != 3):
+            raise AssertionError("wrong number of arguments")
+        elif not sys.argv[2].isdigit():
+            raise AssertionError("second argument must be a number")
+
+        N = int(sys.argv[2])
+        S = sys.argv[1].split()
+        # lambda c;est comme declarer et utiliser une fonction en une ligne
+        # fonction lambda qui prend word et return les mot de N ou plus
+        filtered = ft_filter(lambda word: len(word) > N, S)
+        filtered_list = [word for word in filtered]
+        print(filtered_list)
+
+    except AssertionError as e:
+        print("AssertionError:", e, file=sys.stderr)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Python00/ex07/sos.py
@@ -0,0 +1,74 @@
+import sys
+
+
+def main():
+    """
+    Main block to print the informations on the inpt
+    """
+    NESTED_MORSE = {" ": "/ ",
+                    "A": ".- ",
+                    "B": "-... ",
+                    "C": "-.-. ",
+                    "D": "-.. ",
+                    "E": ". ",
+                    "F": "..-. ",
+                    "G": "--. ",
+                    "H": ".... ",
+                    "I": ".. ",
+                    "J": ".--- ",
+                    "K": "-.- ",
+                    "L": ".-.. ",
+                    "M": "-- ",
+                    "N": "-. ",
+                    "O": "--- ",
+                    "P": ".--. ",
+                    "Q": "--.- ",
+                    "R": ".-. ",
+                    "S": "... ",
+                    "T": "- ",
+                    "U": "..- ",
+                    "V": "...- ",
+                    "W": ".-- ",
+                    "X": "-..- ",
+                    "Y": "-.-- ",
+                    "Z": "--.. ",
+                    "0": "----- ",
+                    "1": ".---- ",
+                    "2": "..--- ",
+                    "3": "...-- ",
+                    "4": "....- ",
+                    "5": "..... ",
+                    "6": "-.... ",
+                    "7": "--... ",
+                    "8": "---.. ",
+                    "9": "----. "}
+    try:
+        if (len(sys.argv) != 2):
+            raise AssertionError("wrong number of arguments.")
+
+    except AssertionError as e:
+        print("AssertionError:", e, file=sys.stderr)
+        return 0
+
+    input_txt = sys.argv[1]
+    input_txt = input_txt.upper()
+
+    is_valid = True
+    for c in input_txt:
+        if not (c.isalnum() or c == ' '):
+            is_valid = False
+            break
+
+    if not is_valid:
+        raise AssertionError("must contain only letters, digits or spaces.")
+
+    morse_trad = ""
+    for char in input_txt:
+        morse_trad += NESTED_MORSE[char]
+
+    print(morse_trad)
+    return 0
+
+
+if __name__ == "__main__":
+    main()
