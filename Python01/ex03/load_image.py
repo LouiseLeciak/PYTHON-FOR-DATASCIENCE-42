311a42eb80cf5551adb7648b47f26b62e0525138
lleciak <leciak.louise@gmail.com>
2025-07-31 10:02:51 +0200

    CORRECTION + EX03

--- Python01/ex03/load_image.py
+++ Python01/ex03/load_image.py
@@ -2,10 +2,6 @@ from PIL import Image
 import numpy as np
 
 def ft_load(path: str) -> np.ndarray:
-    """
-    A fonction that loads an image, prints its format and its
-    pixels content in RGB format
-    """
     try:
         img = Image.open(path)
     except:
@@ -13,7 +9,6 @@ def ft_load(path: str) -> np.ndarray:
     if img.format not in ('JPEG', 'JPG'):
         raise TypeError("not a valid format")
     img_np = np.array(img)
-    print(f"format: {img.format}")
     print(f"The shape of image is: {img_np.shape}")
     print(img_np)
     return img_np
